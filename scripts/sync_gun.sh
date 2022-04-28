2bc7f42fab55508bc7c9e82fa7ed300dc1421242
Xinchun Liu <xcliu@ca.ibm.com>
2022-04-28 07:56:00 -0400

    gunbump

--- scripts/sync_gun.sh
+++ scripts/sync_gun.sh
@@ -16,7 +16,7 @@ cp -av /Users/xcliu/tests/gun-vue/app/src/components/*  src/components/
 rm -frv src/pages/*
 cp -av /Users/xcliu/tests/gun-vue/app/src/pages/*  src/pages/
 # cp -av /Users/xcliu/tests/gun-vue/app/public/*  public/
-git checkout src/pages/videos.vue src/pages/wallets.vue src/pages/upload.vue
+git checkout src/pages/videos.vue src/pages/wallets.vue src/pages/upload.vue src/pages/nft src/pages/nfts.vue
 
 cp -av /Users/xcliu/tests/gun-vue/app/src/main.js src/main.ts
 cp -av /Users/xcliu/tests/gun-vue/app/src/app.vue src/App.vue
