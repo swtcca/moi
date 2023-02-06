270695265e20c7b03bf8ded80d8fd8b6e17a7de1
Xinchun Liu <xcliu@ca.ibm.com>
2023-02-06 11:48:50 -0500

    homepage

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,12 +1,30 @@
-<script setup>
-import { currentRoom, rootRoom } from '#composables';
+<script setup lang="ts">
+import { useGun, currentRoom, rootRoom } from '../gun-vue/composables'
+import { initChannels } from "../composables/useVideos"
+
+globalThis.gun = useGun()
+// import * as GunComposable from '../gun-vue/composables'
+// globalThis.GunComposable = GunComposable
+
+onBeforeMount(() => {
+  const {vref, cref, gvideos, gchannels, pref, pvideos, tvideos, tref} = initChannels()
+  globalThis.pvideos = pvideos
+  globalThis.pref = pref
+  globalThis.gvideos = gvideos
+  globalThis.vref = vref
+  globalThis.gchannels = gchannels
+  globalThis.cref = cref
+  globalThis.tvideos = tvideos
+  globalThis.tref = tref
+})
+const { t } = useI18n()
 </script>
 
 <template lang="pug">
 .flex.flex-col
   room-page(
     :key="currentRoom.pub" 
-    @browse="$router.push(`/${$event}/`)"
+    @browse="$router.push(`/${$event}/`)" 
     )
 
 </template>
