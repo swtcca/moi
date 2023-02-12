4af0226e4398f6879dbac9ecc4e2795a1b0aed75
Xinchun Liu <xcliu@ca.ibm.com>
2023-02-11 20:31:44 -0500

    space

--- vite.config.ts
+++ vite.config.ts
@@ -89,7 +89,7 @@ export default defineConfig({
           },
         }),
         presetTagify(),
-        presetAttributify(),
+        // presetAttributify(),
         presetUno(),
         presetTypography(),
         presetForms()
