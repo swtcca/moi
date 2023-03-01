018838b3deb9a3ab01277eb2d6dab822c5cfcb16
Xinchun Liu <xcliu@ca.ibm.com>
2023-03-01 13:07:44 -0500

    nav

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,3 +1,22 @@
+<script setup lang="ts">
+import { globalState } from '../stores/globalState'
+import { useUser } from '../gun-vue/composables'
+const { top } = useScreenSafeArea()
+const { user } = useUser()
+const navier = ref(null)
+const move_ref = ref(null)
+const home_ref = ref(null)
+const user_ref = ref(null)
+const { height, width } = useWindowSize()
+const { x, y, style } = useDraggable(move_ref, {initialValue: {x: 0, y:0 }})
+console.log(`${width.value} x ${height.value}`)
+console.log(style.value)
+globalState.layout_classes = computed(() => `mt-${top.value}`)
+const home_classes = computed(() => `left:${x.value}px;bottom:${y.value}px`)
+const user_classes = computed(() => `right:${x.value}px;bottom:${y.value}px`)
+// globalState.nav_classes = computed(() => globalState.show_top ? `mt-${Math.floor(heightNavTop.value || 0) + parseInt(top.value.replace(/px/,"") || "0")}px` : `mb-${Math.floor(heightNavBottom.value)}px`)
+</script>
+
 <template>
   <main class="h-screen">
     <div ref="navier" class="fixed left-0 top-0 z-400 inset-x-0 opacity-90">
@@ -17,7 +36,7 @@
         <div class="i-la-home text-6xl  text-blue-700 text-opacity-90 text-stroke-sm text-stroke-green-700"></div>
       </RouterLink>
     </div>
-    <div ref="user_ref" class="fixed z-400" :style="user_classes">
+    <div ref="user_ref" v-show="$route.path === '/' || !user.is " class="fixed z-400" :style="user_classes">
       <UserIcon
         :size="48"
         :showName="false"
@@ -28,20 +47,3 @@
     </div>
   </main>
 </template>
-
-<script setup lang="ts">
-import { globalState } from '../stores/globalState'
-const { top } = useScreenSafeArea()
-const navier = ref(null)
-const move_ref = ref(null)
-const home_ref = ref(null)
-const user_ref = ref(null)
-const { height, width } = useWindowSize()
-const { x, y, style } = useDraggable(move_ref, {initialValue: {x: 0, y:0 }})
-console.log(`${width.value} x ${height.value}`)
-console.log(style.value)
-globalState.layout_classes = computed(() => `mt-${top.value}`)
-const home_classes = computed(() => `left:${x.value}px;bottom:${y.value}px`)
-const user_classes = computed(() => `right:${x.value}px;bottom:${y.value}px`)
-// globalState.nav_classes = computed(() => globalState.show_top ? `mt-${Math.floor(heightNavTop.value || 0) + parseInt(top.value.replace(/px/,"") || "0")}px` : `mb-${Math.floor(heightNavBottom.value)}px`)
-</script>
