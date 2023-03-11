d6c65217310bee95b3e540c9ea7e2598901e1b72
Xinchun Liu <xcliu@ca.ibm.com>
2023-03-11 15:06:59 -0500

    navier

--- src/components/Navier.vue
+++ src/components/Navier.vue
@@ -41,34 +41,98 @@ watch(class_font, () => console.log(class_font.value))
 //    <button class="i-ph-playlist" v-if="route_videos" :title="t('button.playlist')" @click="() => togglePlayList()">
 //    </button>
 
+//  <nav ref="navier" class="grid-row grid-cols-4 md:grid-cols-6 lg:grid-cols-8 xl:grid-cols-10 text-4xl place-items-center bg-light-300 dark-bg-dark-400">
+//    <button class="i-ph-sun dark:i-ph-moon" :title="t('button.toggle_dark')" @click="() => toggleDark()">
+//    </button>
+//    <button class="i-mdi-fullscreen" :title="t('button.toggle_fullscreen')" @click="() => toggle()">
+//    </button>
+//    <button ref="language" class="i-ph-translate" :title="t('button.toggle_langs')" @click="toggleLocales">
+//    </button>
+//    <button ref="font" class="i-carbon-text-font" :title="t('button.toggle_fonts')" @click="toggleFonts">
+//    </button>
+//    <button class="i-ph-gear" :title="t('button.settings')" @click="show.settings = !show.settings">
+//    </button>
+//    <router-link to="/videos/" class="i-ph-video-camera"></router-link>
+//    <router-link to="/openai/" class="i-logos-openai-icon"></router-link>
+//    <button class="i-mdi-graph-outline" v-if="/localhost|127.0.0.1/.test(location_origin)" :title="t('button.graph')" @click="show.graph = !show.graph">
+//    </button>
+//    <button :title="t('button.relays')">
+//      <GunRelay />
+//    </button>
+//    <router-link to="/upload/" v-if="false" class="i-simple-icons-ipfs">
+//    </router-link>
+//    <router-link to="/wallets/" class="i-ph-wallet">
+//    </router-link>
+//    <router-link to="/nfts/">
+//      NFT
+//    </router-link>
+//  </nav>
 </script>
 
 <template>
-  <nav ref="navier" class="flex justify-around sm-px-8 md-px-16 lg-px-32 items-center text-center bg-cyan-300 dark-bg-cyan-600 text-xl py-1 mx-auto">
-    <button class="i-ph-sun dark:i-ph-moon" :title="t('button.toggle_dark')" @click="() => toggleDark()">
-    </button>
-    <button class="i-mdi-fullscreen" :title="t('button.toggle_fullscreen')" @click="() => toggle()">
-    </button>
-    <button ref="language" class="i-ph-translate" :title="t('button.toggle_langs')" @click="toggleLocales">
-    </button>
-    <button ref="font" class="i-carbon-text-font" :title="t('button.toggle_fonts')" @click="toggleFonts">
-    </button>
-    <button class="i-ph-gear" :title="t('button.settings')" @click="show.settings = !show.settings">
-    </button>
-    <router-link to="/videos/" class="i-ph-video-camera"></router-link>
-    <router-link to="/openai/" class="i-logos-openai-icon"></router-link>
-    <button class="i-mdi-graph-outline" v-if="/localhost|127.0.0.1/.test(location_origin)" :title="t('button.graph')" @click="show.graph = !show.graph">
-    </button>
-    <button :title="t('button.relays')">
-      <GunRelay />
-    </button>
-    <router-link to="/upload/" v-if="false" class="i-simple-icons-ipfs">
-    </router-link>
-    <router-link to="/wallets/" class="i-ph-wallet">
-    </router-link>
-    <router-link to="/nfts/">
-      NFT
-    </router-link>
+  <nav ref="navier" class="flex flex-col items-center bg-light-300 dark-bg-dark-400">
+    <div class="flex flex-wrap items-center gap-2 p-4">
+      <div class="cursor-pointer flex-1 flex flex-col items-center p-4 relative dark-bg-dark-200 rounded-lg shadow-sm transition hover-shadow-lg" :title="t('button.toggle_dark')" @click="() => toggleDark()">
+        <div class="text-2xl">
+          <div class="i-ph-sun"></div>
+        </div>
+      </div>
+      <div class="cursor-pointer flex-1 flex flex-col items-center p-4 relative dark-bg-dark-200 rounded-lg shadow-sm transition hover-shadow-lg" :title="t('button.toggle_fullscreen')" @click="() => toggle()">
+        <div class="text-2xl">
+          <div class="i-mdi-fullscreen"></div>
+        </div>
+      </div>
+      <div class="cursor-pointer flex-1 flex flex-col items-center p-4 relative dark-bg-dark-200 rounded-lg shadow-sm transition hover-shadow-lg" :title="t('button.toggle_langs')" @click="toggleLocales">
+        <div class="text-2xl">
+          <div class="i-ph-translate"></div>
+        </div>
+      </div>
+      <div class="cursor-pointer flex-1 flex flex-col items-center p-4 relative dark-bg-dark-200 rounded-lg shadow-sm transition hover-shadow-lg" :title="t('button.toggle_fonts')" @click="toggleFonts">
+        <div class="text-2xl">
+          <div class="i-carbon-text-font"></div>
+        </div>
+      </div>
+      <div class="cursor-pointer flex-1 flex flex-col items-center p-4 relative dark-bg-dark-200 rounded-lg shadow-sm transition hover-shadow-lg" :title="t('button.settings')" @click="show.settings = !show.settings">
+        <div class="text-2xl">
+          <div class="i-ph-gear"></div>
+        </div>
+      </div>
+      <router-link to="/videos/" class="cursor-pointer flex-1 flex flex-col items-center p-4 relative dark-bg-dark-200 rounded-lg shadow-sm transition hover-shadow-lg" >
+        <div class="text-2xl">
+          <div class="i-ph-video-camera"></div>
+        </div>
+      </router-link>
+      <router-link to="/openai/" class="cursor-pointer flex-1 flex flex-col items-center p-4 relative dark-bg-dark-200 rounded-lg shadow-sm transition hover-shadow-lg" >
+        <div class="text-2xl">
+          <div class="i-logos-openai-icon"></div>
+        </div>
+      </router-link>
+      <div v-if="/localhost|127.0.0.1/.test(location_origin)" class="cursor-pointer flex-1 flex flex-col items-center p-4 relative dark-bg-dark-200 rounded-lg shadow-sm transition hover-shadow-lg" :title="t('button.graph')" @click="show.graph = !show.graph">
+        <div class="text-2xl">
+          <div class="i-mdi-graph-outline"></div>
+        </div>
+      </div>
+      <div class="cursor-pointer flex-1 flex flex-col items-center p-4 relative dark-bg-dark-200 rounded-lg shadow-sm transition hover-shadow-lg" :title="t('button.relays')">
+        <div class="text-2xl">
+          <GunRelay />
+        </div>
+      </div>
+      <router-link to="/upload/" v-if="false" class="cursor-pointer flex-1 flex flex-col items-center p-4 relative dark-bg-dark-200 rounded-lg shadow-sm transition hover-shadow-lg" >
+        <div class="text-2xl">
+          <div class="i-simple-icons-ipfs"></div>
+        </div>
+      </router-link>
+      <router-link to="/wallets/" class="cursor-pointer flex-1 flex flex-col items-center p-4 relative dark-bg-dark-200 rounded-lg shadow-sm transition hover-shadow-lg">
+        <div class="text-2xl">
+          <div class="i-ph-wallet"></div>
+        </div>
+      </router-link>
+      <router-link to="/nfts/" class="cursor-pointer flex-1 flex flex-col items-center p-4 relative dark-bg-dark-200 rounded-lg shadow-sm transition hover-shadow-lg">
+        <div class="text-2xl">
+        NFT
+        </div>
+      </router-link>
+    </div>
   </nav>
   <UiLayer :open="show.graph" @close="show.graph=false">
     <GunGraph />
