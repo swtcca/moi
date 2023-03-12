a231101686e5eb9e769f7c1a8836e03a85ae9e76
Xinchun Liu <xcliu@ca.ibm.com>
2023-03-11 21:32:45 -0500

    contract

--- src/components/Nft.vue
+++ src/components/Nft.vue
@@ -21,14 +21,12 @@ const load_more = async () => {
 
 <template lang='pug'>
 .container.overflow-x-hidden
-  .flex.flex-col.gap-2.max-w-full.sm-max-w-sm.bg-gray-200.dark-bg-gray-400.shadow-lg.mx-auto.rounded.mb-1(v-if="contract.total_supply > 0")
-    .flex.justify-center.items-center.gap-4.text-blue-600.bg-gray-200.mx-auto
-      p {{ standard }}
-      p {{ chain }}
-      p {{ contract.name }}
-    .mx-auto total {{ contract.total_supply }}
-    .font-sm.font-extralight.tracking-tighter.overflow-truncate.px-1.mx-auto
-      p {{ address }}
+  ACard(
+    v-if="contract.total_supply > 0"
+    :title="contract.name + ' (' + standard + ' - ' + chain + ')'"
+    :text="address"
+    :subtitle="'total ' + contract.total_supply"
+  )
   .bg-white.dark-bg-gray-800.grid.grid-col.grid-cols-1.gap-2.min-w-xs.place-content-center.sm-px-2.sm-gap-4.md-px-4.md-gap-6.md-grid-cols-2.lg-gap-8.lg-grid-cols-3.xl-grid-cols-4.text-center.text-gray-700.dark-text-gray-200(v-if="list_tokens")
     div(v-for="(token,index) in contract.tokens" :key="index")
       NftToken(:token="token" :contract="{address, chain, standard}")
