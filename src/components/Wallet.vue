ad58189dd5ce842321c4d694f9fd2b7419a9c7da
Xinchun Liu <xcliu@ca.ibm.com>
2023-03-11 21:09:39 -0500

    layout

--- src/components/Wallet.vue
+++ src/components/Wallet.vue
@@ -69,55 +69,56 @@ const { t } = useI18n()
 </script>
 
 <template lang="pug">
-h1.pt-8.text-center.font-bold.text-2xl Wallets
-.container.overflow-x-hidden
-  .grid.grid-col-1.place-content-around.gap-2.p-4.min-h-60vh
-    .flex.flex-col.gap-2.max-w-full.sm-max-w-sm.rounded-xl.shadow-lg
-      Listbox(v-model="selectedChain")
-        .relative.mt-1
-          ListboxButton(class="relative w-full py-2 pl-12 pr-10 text-left rounded-lg shadow-md cursor-default focus-outline-none focus-visible-ring-2 focus-visible-ring-opacity-75 focus-visible-ring-white focus-visible-ring-offset-orange-300 focus-visible-ring-offset-2 focus-visible-border-indigo-500 sm-text-sm")
-            span.block.truncate {{ selectedChain.name }}
-            span.absolute.inset-y-0.right-0.flex.items-center.pr-2.pointer-events-none
-              ChevronUpDownIcon.w-5.h-5(aria-hidden="true")
-  
-          ListboxOptions(class="absolute w-full py-1 mt-1 overflow-auto text-base rounded-md shadow-lg max-h-60 ring-1 ring-black ring-opacity-5 focus-outline-none sm-text-sm")
-            ListboxOption(
-              v-slot="{ active, selected }"
-              v-for="chain in chain_names"
-              :key="chain.name"
-              :value="chain"
-              as="template"
-            )
-              li.cursor-default.select-none.relative.py-2.pl-10.pr-4(
+.container.h-100vh
+  h1.pt-8.text-center.font-bold.text-2xl Wallets
+  .container.overflow-x-hidden
+    .grid.grid-col-1.place-content-around.gap-2.p-4.min-h-85vh
+      .flex.flex-col.gap-2.max-w-full.sm-max-w-sm.rounded-xl.shadow-lg(v-if="user.is")
+        .flex.justify-center.items-center.gap-4
+          p {{ wallet.chain }}
+          .i-mdi-wallet.w-24.h-24
+          p {{ wallet.algorithm }}
+        .mx-auto(v-if="balances[chain]?.native?.value") {{ t('wallets.balance') }} {{ balances[chain]?.native?.value }} {{ balances[chain]?.native?.token }}
+        .font-mono.tracking-tight.px-1.mx-auto {{ wallet.address }}
+        .flex.flex-col.gap-2(v-if="user.safe.wallets[chain]?.activated")
+          .border-b
+          div(v-if="Object.keys(balances[chain].tokens).length > 0")
+            p {{ t('wallets.tokens') }}
+            ul.pl-4(v-for="(value, token) of balances[chain].tokens" :key="token")
+              li {{ value.value }} {{ token }}
+          .flex.place-content-around
+            button.button
+              .i-ph-spinner-bold(v-if="wallet.querying" @click="noop()")
+              .i-carbon-update-now(v-else @click="wallet.chainobj.update_balance(wallet)")
+        div(v-else)
+          .flex.place-content-around
+            p {{ t('wallets.not_activated') }}
+            button.button(@click="wallet.chainobj.update_balance(wallet)")
+              .i-ph-spinner-bold(v-if="wallet.querying" @click="noop()")
+              .i-carbon-update-now(v-else @click="wallet.chainobj.update_balance(wallet)")
+      .flex.flex-col.gap-2.max-w-full.sm-max-w-sm.p-2.rounded-xl.shadow-lg(v-else)
+        p {{ t('wallets.login_first') }}
+      .flex.flex-col.gap-2.max-w-full.sm-max-w-sm.rounded-xl.shadow-lg
+        Listbox(v-model="selectedChain")
+          .relative.mt-1
+            ListboxButton(class="relative w-full py-2 pl-12 pr-10 text-left rounded-lg shadow-md cursor-default focus-outline-none focus-visible-ring-2 focus-visible-ring-opacity-75 focus-visible-ring-white focus-visible-ring-offset-orange-300 focus-visible-ring-offset-2 focus-visible-border-indigo-500 sm-text-sm")
+              span.block.truncate {{ selectedChain.name }}
+              span.absolute.inset-y-0.right-0.flex.items-center.pr-2.pointer-events-none
+                ChevronUpDownIcon.w-5.h-5(aria-hidden="true")
+    
+            ListboxOptions(class="absolute w-full py-1 mt-1 overflow-auto text-base rounded-md shadow-lg max-h-60 ring-1 ring-black ring-opacity-5 focus-outline-none sm-text-sm")
+              ListboxOption(
+                v-slot="{ active, selected }"
+                v-for="chain in chain_names"
+                :key="chain.name"
+                :value="chain"
+                as="template"
               )
-                span.block.truncate(
-                  :class="{ 'font-medium' : selected, 'font-normal': !selected }"
-                ) {{ chain.name }}
-                span.absolute.inset-y-0.left-0.flex.items-center.pl-3(v-if="selected")
-                  CheckIcon.w-5.h-5(aria-hidden="true")
-    .flex.flex-col.gap-2.max-w-full.sm-max-w-sm.rounded-xl.shadow-lg(v-if="user.is")
-      .flex.justify-center.items-center.gap-4
-        p {{ wallet.chain }}
-        .i-mdi-wallet.w-12.h-12
-        p {{ wallet.algorithm }}
-      .mx-auto(v-if="balances[chain]?.native?.value") {{ t('wallets.balance') }} {{ balances[chain]?.native?.value }} {{ balances[chain]?.native?.token }}
-      .font-mono.tracking-tight.px-1.mx-auto {{ wallet.address }}
-      .flex.flex-col.gap-2(v-if="user.safe.wallets[chain]?.activated")
-        .border-b
-        div(v-if="Object.keys(balances[chain].tokens).length > 0")
-          p {{ t('wallets.tokens') }}
-          ul.pl-4(v-for="(value, token) of balances[chain].tokens" :key="token")
-            li {{ value.value }} {{ token }}
-        .flex.place-content-around
-          button.button
-            .i-ph-spinner-bold(v-if="wallet.querying" @click="noop()")
-            .i-carbon-update-now(v-else @click="wallet.chainobj.update_balance(wallet)")
-      div(v-else)
-        .flex.place-content-around
-          p {{ t('wallets.not_activated') }}
-          button.button(@click="wallet.chainobj.update_balance(wallet)")
-            .i-ph-spinner-bold(v-if="wallet.querying" @click="noop()")
-            .i-carbon-update-now(v-else @click="wallet.chainobj.update_balance(wallet)")
-    .flex.flex-col.gap-2.max-w-full.sm-max-w-sm.p-2.rounded-xl.shadow-lg(v-else)
-      p {{ t('wallets.login_first') }}
+                li.cursor-default.select-none.relative.py-2.pl-10.pr-4(
+                )
+                  span.block.truncate(
+                    :class="{ 'font-medium' : selected, 'font-normal': !selected }"
+                  ) {{ chain.name }}
+                  span.absolute.inset-y-0.left-0.flex.items-center.pl-3(v-if="selected")
+                    CheckIcon.w-5.h-5(aria-hidden="true")
 </template>
