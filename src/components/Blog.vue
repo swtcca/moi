733ad17eb6b1f6d191976cce1c15dbf1f96f1510
Xinchun Liu <xcliu@ca.ibm.com>
2023-03-29 21:22:34 -0400

    dark

--- src/components/Blog.vue
+++ src/components/Blog.vue
@@ -11,11 +11,11 @@ const props = defineProps({
 const blog = computed(() => blogs.value.find(post => post.id === props.id))
 </script>
 <template>
-  <div class="sm:p-4 md:p-8 lg:p-16 xl:p32">
+  <div class="sm:p-4 md:p-8 lg:p-16 xl:p32 bg-light-200 dark:bg-dark-500">
     <h1>{{ blog.title }}</h1>
-    <div v-if="!edit" class="markdown-body prose" v-html="md.render(blog.content)"></div>
-    <div v-if="edit">
-      <textarea class="w-full h-75vh" v-model="blog.content" />
+    <div v-if="!edit" class="markdown-body prose dark:prose-invert" v-html="md.render(blog.content)"></div>
+    <div v-if="edit"  class="bg-light-200 dark:bg-dark-500">
+      <textarea class="w-full h-75vh  bg-light-200 dark:bg-dark-500" v-model="blog.content" />
     </div>
     <p>By {{ blog.author }} on {{ blog.date }}</p>
     <button v-if="edit" class="i-ph-floppy-disk text-4xl" @click.prevent="edit=!edit"></button>
