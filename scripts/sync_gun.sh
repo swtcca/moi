e5121a9cdaad0122666869a8f058633d34521a32
Xinchun Liu <xcliu@ca.ibm.com>
2023-04-05 19:29:04 -0400

    sync

--- scripts/sync_gun.sh
+++ scripts/sync_gun.sh
@@ -4,5 +4,7 @@ rm -fr src/gun-vue/*
 cp -av /Users/xcliu/tests/gun-vue/src/* src/gun-vue
 mv src/gun-vue/account/AccountStars.vux src/gun-vue/account/AccountStars.vue || true
 rm -frv src/gun-vue/*/*story.vu*
+rm -frv src/gun-vue/app
+rm -frv src/gun-vue/{main.ts,app.vue}
 
-cp -av /Users/xcliu/tests/gun-vue/app/src/components/*  src/components/
+cp -av /Users/xcliu/tests/gun-vue/src/app/*  src/components/
