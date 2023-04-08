7de52614969404f906fc1b2b9acf9dfcd3d47ba6
Xinchun Liu <xcliu@ca.ibm.com>
2023-04-07 23:39:25 -0400

    rebuild

--- scripts/sync_gun.sh
+++ scripts/sync_gun.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 rm -fr src/gun-vue/*
+find /Users/xcliu/tests/gun-vue/src -name "*.md" | xargs rm -f
 cp -av /Users/xcliu/tests/gun-vue/src/* src/gun-vue
 mv src/gun-vue/account/AccountStars.vux src/gun-vue/account/AccountStars.vue || true
-rm -frv src/gun-vue/*/*story.vu*
 rm -frv src/gun-vue/app
-rm -frv src/gun-vue/{main.ts,app.vue}
+# rm -frv src/gun-vue/{main.ts,app.vue}
 
 cp -av /Users/xcliu/tests/gun-vue/src/app/*  src/components/
--- src/gun-vue/chat/README.md
+++
@@ -1,3 +0,0 @@
-## Public Chat
-
-Public chat is happening in a particular room, that enables them. Any user with a certificate can start a topic. Then anyone can start writing into it. **General** is considered the starting point, but due to the nature of public chat it won't be useable in case there are thousands of random people. But going deeper with the topics is a way to expand the conversations and make them more productive.
